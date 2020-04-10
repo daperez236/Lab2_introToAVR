2930e709ca7b2ec171c5350fc6b7154c42498263
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-04-10 00:11:10 -0700

    done with examples

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: dpere048
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 021
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,10 +14,24 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF; //CONFIGURE PORT A 8PINS AS INPUTS
+	DDRB = 0xFF; PORTB = 0x00; //CONFIGURE PORT B 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; //temp var to hold value of B
+	unsigned char tmpA = 0x00; //tmp var to hold value of A
     /* Insert your solution below */
     while (1) {
-
+	tmpA = PINA&0x01;
+	if (tmpA == 0x01){
+		tmpB = (tmpB & 0xFC) | 0x01; //tmpB = bbbbbb01
+					//clears rightmost 2 bits, sets to 01
+	}else{
+		tmpB = (tmpB &0xFC) | 0x02; //bbbbbb10
+					//clear right 2 bits, set to 10
+	}
+ 
+	PORTB = tmpB;
     }
     return 1;
 }
+
+
